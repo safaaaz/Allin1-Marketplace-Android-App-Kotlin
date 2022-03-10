49c8d669580c179540e6d6383aa6db179bfe2280
shatha <shathab@ac.sce.ac.il>
2022-03-10 17:55:32 +0200

    add product in new page

--- app/src/main/java/com/safaashatha/allin1/ownersignup.kt
+++ app/src/main/java/com/safaashatha/allin1/ownersignup.kt
@@ -17,7 +17,10 @@ import android.widget.LinearLayout
 
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_addprod.*
 import kotlinx.android.synthetic.main.fragment_store_add.*
+import kotlinx.android.synthetic.main.fragment_store_add.Catagory
+import kotlinx.android.synthetic.main.fragment_store_add.name
 
 class ownersignup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +37,7 @@ class ownersignup : AppCompatActivity() {
 
 
     fun savestore(view: View){
+        //val layout2 = findViewById(R.id.layout) as LinearLayout
         FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(owner(name.text.toString()
             ,Address.text.toString(),Catagory.text.toString(),phone.text.toString()),"products")
         //name.setText("")
@@ -41,10 +45,13 @@ class ownersignup : AppCompatActivity() {
             "book","for reading","30"))
 
         FirebaseDatabase.getInstance().reference.child("300").setValue("tt")
+
         view.findNavController().navigate(R.id.action_storeAdd_to_mystore)
     }
 
     fun addproduct(view: View) {
+        view.findNavController().navigate(R.id.action_mystore_to_addprod)
+
         //val button = findViewById<Button>(R.id.Add_product)
 
             // button?.setOnClickListener()
@@ -52,58 +59,46 @@ class ownersignup : AppCompatActivity() {
 
            // Toast.makeText(this@ownersignup, "shatha", Toast.LENGTH_LONG).show() }
 
-        val layout1 = findViewById(R.id.layout) as LinearLayout
-        val name = TextView(this)
-        val editText = EditText(this)
-        val button_add = Button(this)
-        val catagory = TextView(this)
-        val catagory1 = EditText(this)
-        val price = TextView(this)
-        val price1 = EditText(this)
-
-        name.layoutParams= LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        name.text = "Name"
-        editText.setHint("Enter name product")
-        editText.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        editText.setPadding(20, 20, 20, 20)
-
-        catagory.layoutParams= LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        catagory.text = "Catagory"
-
-        catagory1.setHint("Enter Catagory")
-        catagory1.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        catagory1.setPadding(20, 20, 20, 20)
-
-        price.layoutParams= LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        price.text = "Price"
-        price1.setHint("Enter Price")
-        price1.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        price1.setPadding(20, 20, 20, 20)
+        //val layout1 = findViewById(R.id.layout) as LinearLayout
+        //val name = TextView(this)
 
 
-        button_add.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        button_add.text = "Add Product"
+        //name.layoutParams= LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        //name.text = "Name"
 
-        layout1.addView(name)
-        layout1.addView(editText)
-        layout1.addView(catagory)
-        layout1.addView(catagory1)
-        layout1.addView(price)
-        layout1.addView(price1)
-        layout1.addView(button_add)
+        //layout1.addView(name)
+
         //save_button.setOnClickListener{FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(FirebaseAuth.getInstance().currentUser!!.uid+"/products").child("1").setValue(product(
           //  "book","for reading","30"))}
-        button_add.setOnClickListener()
-        {
+        //button_add.setOnClickListener()
+        //{
 
+        //Toast.makeText(this@ownersignup, "The product is add", Toast.LENGTH_LONG).show()
+          //  FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(FirebaseAuth.getInstance().currentUser!!.uid+"/products").child(editText.text.toString()).setValue(product(
+              //  editText.text.toString(),price1.text.toString(),catagory1.text.toString()
+            //))
+        //}
+
+
+    }
+    fun addprod(view: View){
+
+
+        FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(FirebaseAuth.getInstance().currentUser!!.uid+"/products").child(name.text.toString()).setValue(product(
+             name.text.toString(),price.text.toString(),Catagory.text.toString()))
         Toast.makeText(this@ownersignup, "The product is add", Toast.LENGTH_LONG).show()
-            FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(FirebaseAuth.getInstance().currentUser!!.uid+"/products").child("1").setValue(product(
-                editText.text.toString(),price1.text.toString(),catagory1.text.toString()
-            ))
-        }
+        view.findNavController().navigate(R.id.action_addprod_to_mystore)
+
+
+    }
+    fun Cancel(view: View){
+
+
+        view.findNavController().navigate(R.id.action_addprod_to_mystore)
 
 
     }
 
-    }
+
+}
 
--- app/src/main/java/com/safaashatha/allin1/product.kt
+++ app/src/main/java/com/safaashatha/allin1/product.kt
@@ -1,20 +1,34 @@
 package com.safaashatha.allin1
 
+import androidx.annotation.DrawableRes
+
+
+
+
 class product {
     var name:String?=null
-
     var price:String=""
     var category:String=""
 
+    
+
+
     constructor(
         name:String,
-
         price:String="",
-        category:String=""){
+        category:String="",
+
+    )
+    {
         this.name=name
         this.price=price
         this.category=category
 
 
+    }
+     fun printproduct(){
+        print(this.name)
+        print(this.category)
+        print(this.price)
     }
 }
