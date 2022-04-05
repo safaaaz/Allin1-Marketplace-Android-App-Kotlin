eb4a11838343a1488bf62f3d17c8fe0d5a55f668
shatha <shathab@ac.sce.ac.il>
2022-04-05 23:04:31 +0300

    show the products in  main page

--- app/src/main/java/com/safaashatha/allin1/MainActivity.kt
+++ app/src/main/java/com/safaashatha/allin1/MainActivity.kt
@@ -19,6 +19,10 @@ import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.fragment_blank.*
 
 import android.R.string.no
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.google.firebase.database.*
 
 
@@ -34,93 +38,10 @@ class MainActivity : AppCompatActivity() {
         val emailid=intent.getStringExtra("email_id")
 
         binding=ActivityMainBinding.inflate(layoutInflater)
-        //user_id.text="user id :: $userid"
-        //email_id.text="email id:: $emailid"
-
-        //productsarrylist.add(product("apple","20","food",R.drawable.googleg_standard_color_18))
         readData()
         val reference = FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("shops")
-/*
-        reference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                productsarrylist=ArrayList()
-                for (postSnapshot in dataSnapshot!!.children) {
-                    productsarrylist.remove(c)
-                    val post = dataSnapshot.child("products")
-                    for (productsnapshot in post.children){
-                        val product=productsnapshot.getValue<product>()
-                        productsarrylist.add(product!!)
-                    }
 
 
-                }
-                val post = dataSnapshot.getValue<owner>()
-                // ...
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                println("The read failed: " + databaseError.code)
-            }
-        })*/
-        //val x=reference.get()
-        //productsarrylist=ArrayList()
-        /*
-        reference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                Log.e("firebaseeeeeeeee", "onDataChange ${snapshot.value.toString()}" )
-                productsarrylist.add(product("apple1","20","food",R.drawable.googleg_standard_color_18))
-
-                for (postSnapshot in snapshot!!.children) {
-                    productsarrylist.remove(c)
-                    val post = postSnapshot.child("products")
-                    productsarrylist.add(product("apple2","20","food",R.drawable.googleg_standard_color_18))
-
-                    for (productsnapshot in post.children){
-                        val product=productsnapshot.getValue<product>()
-                        productsarrylist.add(product!!)
-                        productsarrylist.add(product("apple3","20","food",R.drawable.googleg_standard_color_18))
-                    }
-
-
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("firebasefffff", "onCancelled ${error.message}" )
-
-            }
-        })
-        reference.get().addOnCompleteListener { task ->
-            productsarrylist.add(product("apple1","20","food",R.drawable.googleg_standard_color_18))
-            if (task.isSuccessful) {
-                val snapshot = task.result
-                productsarrylist.add(product("apple1","20","food",R.drawable.googleg_standard_color_18))
-
-                for (postSnapshot in snapshot!!.children) {
-                    productsarrylist.remove(c)
-                    val post = postSnapshot.child("products")
-                    productsarrylist.add(product("apple2","20","food",R.drawable.googleg_standard_color_18))
-
-                    for (productsnapshot in post.children){
-                        val product=productsnapshot.getValue<product>()
-                        productsarrylist.add(product!!)
-                        productsarrylist.add(product("apple3","20","food",R.drawable.googleg_standard_color_18))
-                    }
-
-
-                }
-            } else {
-                Log.d("TAGGGGG", task.exception!!.message!!) //Don't ignore potential errors!
-            }
-        }
-        // ...*/
-            //.addOnSuccessListener {
-          //  Log.i("firebase", "Got value ${it.value}")}
-        //reference.addValueEventListener(productListener)
-        //binding.products.isClickable=true
-        //binding.products.setOnItemClickListener{parent, view, position, id->
-        //    val name
-        //}
 
         logoutuser.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -129,45 +50,65 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-
-
     }
     fun readData() {
-        productsarrylist= ArrayList()
+        productsarrylist = ArrayList()
         val c=product("book","30","learning",R.drawable.img)
         productsarrylist.add(c)
-        print("6666666666666666666666666666666666666666666666666666666666")
-        database = FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("shops")
 
+        database =
+            FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("shops")
         database.get().addOnSuccessListener {
-        for (x in it.children)
+            for (x in it.children)
 
-            if (x.exists()) {
-                for(prod in x.child("products").children){
-                    //val product=prod.getValue<product>()
-                    println("-------------------------------------------------------------------------------------")
-                    println(prod.value)
-                    println("-------------------------------------------------------------------------------------")
+                if (x.exists()) {
+                    for (prod in x.child("products").children) {
+                        val pr = product(
+                            prod.child("name").value.toString(),
+                            prod.child("price").value.toString(),
+                            prod.child("category").value.toString(),
+                            R.drawable.img
+                        )
+                        productsarrylist.add(pr)
+                        //println("-######################################################################shatha  "+ productsarrylist.size)
 
-                    productsarrylist.add(product(prod.child("name").value.toString(),prod.child("price").value.toString(),prod.child("category").value.toString(),R.drawable.googleg_standard_color_18))
-
-
+                    }
                 }
-                //val firstname = x.child("products").value
-
-
-            }
-    }
-        for (i in productsarrylist){
-            println("-------------------------------------------------------------------------------------")
-            print(i)
-            println("-------------------------------------------------------------------------------------")
-
-
+            //println("---------------------------------------------------------------------safa  "+ productsarrylist.size)
+            val listView: ListView = findViewById(R.id.products)
+            listView.setAdapter(productadapter(this, productsarrylist))
         }
+        //println("---------------------------------------------------------------------shatha  "+ productsarrylist.size)
 
-        products.adapter= productadapter(this,productsarrylist)
+        //products.adapter = productadapter(this, productsarrylist)
 
-    }
 
-}
+        /*var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto")
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.listview_item, array
+        )
+*/
+
+       /* listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(
+                parent: AdapterView<*>, view: View,
+                position: Int, id: Long
+            ) {
+
+                // value of item that is clicked
+                val itemValue = listView.getItemAtPosition(position) as String
+
+                // Toast the values
+                Toast.makeText(
+                    applicationContext,
+                    "Position :$position\nItem Value : $itemValue", Toast.LENGTH_LONG
+                )
+                    .show()
+            }
+
+        }*/
+
+    }}
