3bd1e4fbcd2c5b35ee402691aab4d0ce4f8fae02
safaaaz <58644583+safaaaz@users.noreply.github.com>
2022-04-08 23:02:20 +0300

    search for product by name

--- app/src/main/java/com/safaashatha/allin1/MainActivity.kt
+++ app/src/main/java/com/safaashatha/allin1/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
-import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -19,10 +18,11 @@ import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.fragment_blank.*
 
 import android.R.string.no
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.*
 import com.google.firebase.database.*
 
 
@@ -111,4 +111,122 @@ class MainActivity : AppCompatActivity() {
 
         }*/
 
-    }}
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu,menu)
+        val search = menu!!.findItem(R.id.prodsearch)
+        val searchView = search.actionView as SearchView
+        searchView.queryHint = "Search"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //products.adapter.filter.filter("hhh")
+                searchforprod(newText!!)
+                return true
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.books -> {
+                Toast.makeText(this,"you click books category",Toast.LENGTH_LONG).show()
+                showproductsbycategory("books")
+                true
+            }
+            R.id.clothes -> {
+                Toast.makeText(this,"you click clothes category",Toast.LENGTH_LONG).show()
+                showproductsbycategory("clothes")
+                true
+            }
+            R.id.nails -> {
+                Toast.makeText(this,"you click nails category",Toast.LENGTH_LONG).show()
+                showproductsbycategory("nails")
+                true
+            }
+            R.id.home -> {
+                Toast.makeText(this,"you click home category",Toast.LENGTH_LONG).show()
+                showproductsbycategory("home")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+    fun showproductsbycategory(cat:String){
+        prodcattext.text=cat
+        productsarrylist = ArrayList()
+        noprod.text=""
+        //val c=product("book","30","learning",R.drawable.img)
+        //productsarrylist.add(c)
+
+        database =
+            FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("shops")
+        database.get().addOnSuccessListener {
+            for (x in it.children)
+
+                if (x.exists()) {
+                    for (prod in x.child("products").children) {
+                        if(prod.child("category").value.toString()==cat) {
+                            val pr = product(
+                                prod.child("name").value.toString(),
+                                prod.child("price").value.toString(),
+                                prod.child("category").value.toString(),
+                                R.drawable.img
+                            )
+                            productsarrylist.add(pr)
+                            println("-######################################################################shatha  "+ productsarrylist.size)
+                        }
+                    }
+                    if(productsarrylist.size==0){
+                        noprod.text="there is no products in this category"
+                    }
+                }
+            //println("---------------------------------------------------------------------safa  "+ productsarrylist.size)
+            val listView: ListView = findViewById(R.id.products)
+            listView.setAdapter(productadapter(this, productsarrylist))
+        }
+    }
+
+    fun searchforprod(newtext:String){
+        prodcattext.text=newtext
+        productsarrylist = ArrayList()
+        noprod.text=""
+        database =
+            FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("shops")
+        database.get().addOnSuccessListener {
+            for (x in it.children)
+
+                if (x.exists()) {
+                    for (prod in x.child("products").children) {
+                        if(newtext in prod.child("name").value.toString()) {
+                            val pr = product(
+                                prod.child("name").value.toString(),
+                                prod.child("price").value.toString(),
+                                prod.child("category").value.toString(),
+                                R.drawable.img
+                            )
+                            productsarrylist.add(pr)
+                            println("-######################################################################shatha  "+ productsarrylist.size)
+                        }
+                    }
+                    if(productsarrylist.size==0){
+                        noprod.text="there is no products in this category"
+                    }
+                }
+            //println("---------------------------------------------------------------------safa  "+ productsarrylist.size)
+            val listView: ListView = findViewById(R.id.products)
+            listView.setAdapter(productadapter(this, productsarrylist))
+
+        }
+    }
+
+}
