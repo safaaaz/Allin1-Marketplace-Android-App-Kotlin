b4399bdd837cb2de7e3955ef6be664fad23b88e9
shatha <shathab@ac.sce.ac.il>
2022-04-25 14:22:30 +0300

    edit, delete and add product edit profile's owner

--- app/src/main/java/com/safaashatha/allin1/ownersignup.kt
+++ app/src/main/java/com/safaashatha/allin1/ownersignup.kt
@@ -1,8 +1,11 @@
 package com.safaashatha.allin1
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
@@ -10,43 +13,103 @@ import android.view.ViewGroup
 import android.view.Gravity
 import android.widget.*
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.auth.FirebaseUser
 
 
 import com.google.firebase.database.FirebaseDatabase
-import com.safaashatha.allin1.databinding.ActivityMainBinding
-import com.safaashatha.allin1.databinding.ActivityOwnersignupBinding
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+//import com.safaashatha.allin1.databinding.ActivityMainBinding
+//import com.safaashatha.allin1.databinding.ActivityOwnersignupBinding
 import com.safaashatha.allin1.databinding.FragmentMystoreBinding
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.logoutuser
 import kotlinx.android.synthetic.main.fragment_addprod.*
+import kotlinx.android.synthetic.main.fragment_addprod.name1
+import kotlinx.android.synthetic.main.fragment_editprof.*
 import kotlinx.android.synthetic.main.fragment_mystore.*
 import kotlinx.android.synthetic.main.fragment_store_add.*
 import kotlinx.android.synthetic.main.fragment_store_add.Catagory
 import kotlinx.android.synthetic.main.fragment_store_add.name
+import kotlinx.android.synthetic.main.fragment_store_add.phone
+import kotlinx.android.synthetic.main.list_products.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ownersignup : AppCompatActivity() {
     private lateinit var productsarrylistt:ArrayList<product>
     private lateinit var database: DatabaseReference
     private lateinit var binding: FragmentMystoreBinding
+    lateinit var file_uri:Uri
+    val REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_mystore)
-
+        setContentView(R.layout.activity_ownersignup)
+        val userid=intent.getStringExtra("user_id")
 
 
         binding = FragmentMystoreBinding.inflate(layoutInflater)
         readData()
-        //if(userid!=null){
+        //openGalleryForImage()
+        if(userid!=null) {
+            val view = findViewById<View>(R.id.Add_store)
+            view.findNavController().navigate(R.id.action_blankFragment3_to_mystore)
+        }
+            //if(userid!=null){
         //  val view=findViewById<View>(R.id.Add_store)
         //view.findNavController().navigate(R.id.action_blankFragment3_to_mystore)
 
 
         //}
+
+    }
+//    private fun openGalleryForImage() {
+//        val intent = Intent(Intent.ACTION_PICK)
+//        intent.type = "image/*"
+//        startActivityForResult(intent, REQUEST_CODE)
+//    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+//            putim.setImageURI(data?.data)
+//             file_uri = data?.data!!
+//            uploadImageToFirebase(file_uri)
+//
+//
+//        }
+//    }
+//    private fun uploadImageToFirebase(fileUri: Uri) {
+//        if (fileUri != null) {
+//            val fileName = UUID.randomUUID().toString() +".jpg"
+//
+//            val database = FirebaseDatabase.getInstance()
+//            val refStorage = FirebaseStorage.getInstance().reference.child("images/$fileName")
+//
+//            refStorage.putFile(fileUri)
+//                .addOnSuccessListener(
+//                    OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
+//                        taskSnapshot.storage.downloadUrl.addOnSuccessListener {
+//                            val imageUrl = it.toString()
+//                        }
+//                    })
+//
+//                ?.addOnFailureListener(OnFailureListener { e ->
+//                    print(e.message)
+//                })
+//        }
+//    }
+
+
+    fun pickpic(view: View){
+       //penGalleryForImage()
+
+
 
     }
 
@@ -139,7 +202,16 @@ class ownersignup : AppCompatActivity() {
                         )
                     )
                     Toast.makeText(this@ownersignup, "The product is add", Toast.LENGTH_LONG).show()
-                    view.findNavController().navigate(R.id.action_addprod_to_mystore)
+
+                //view.findNavController().navigate(R.id.)
+                    val intent = Intent(this, ownersignup::class.java)
+                    intent.flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    intent.putExtra("user_id", FirebaseAuth.getInstance()!!.uid)
+                    //intent.putExtra("email_id", email)
+
+                    startActivity(intent)
+                    finish()
 
 
                 }
@@ -151,12 +223,59 @@ class ownersignup : AppCompatActivity() {
 
 
                 }
+    fun editprofile(view: View){
+        view.findNavController().navigate(R.id.action_mystore_to_editprof)
+    }
+
+    fun editpeoff(view: View){
+
+
+//        FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(
+//            "shops"
+//        ).child(FirebaseAuth.getInstance().currentUser!!.uid).child("address").setValue(addresss.text.toString())
+
+          if( !(TextUtils.isEmpty(name1.text.toString().trim { it <= ' ' })))
+          {
+              FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(
+                  "shops"
+              ).child(FirebaseAuth.getInstance().currentUser!!.uid).child("name").setValue(name1.text.toString())
+
+          }
+
+        if( !(TextUtils.isEmpty(addresss.text.toString().trim { it <= ' ' })))
+        {
+            FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(
+                "shops"
+            ).child(FirebaseAuth.getInstance().currentUser!!.uid).child("address").setValue(addresss.text.toString())
+
+        }
+
+        if( !(TextUtils.isEmpty(phone.text.toString().trim { it <= ' ' })))
+        {
+            FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(
+                "shops"
+            ).child(FirebaseAuth.getInstance().currentUser!!.uid).child("phone").setValue(phone.text.toString())
+
+        }
+
+
+        Toast.makeText(this@ownersignup, "The Profile is updatted ", Toast.LENGTH_LONG).show()
+
+        //view.findNavController().navigate(R.id.)
+        val intent = Intent(this, ownersignup::class.java)
+        intent.flags =
+            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        intent.putExtra("user_id", FirebaseAuth.getInstance()!!.uid)
+        //intent.putExtra("email_id", email)
+
+        startActivity(intent)
+        finish()
+
+    }
+
+
     fun readData() {
         productsarrylistt = ArrayList()
-        //val email = username.text.toString().trim { it <= ' ' }
-        //val pass = password.text.toString().trim { it <= ' ' }
-        //val c=product("book","30","learning",R.drawable.img)
-        //productsarrylist.add(c)
         val userid=intent.getStringExtra("user_id")
         val emailid=intent.getStringExtra("email_id")
 
@@ -186,10 +305,14 @@ class ownersignup : AppCompatActivity() {
                             }
 
                         val listView: ListView = findViewById(R.id.productss)
-                        listView.setAdapter(productadapter(this, productsarrylistt))
+                        listView.setAdapter(productowadapter(this, productsarrylistt))
                     }
 
                 }
+
+
+
+
 
             }
 
--- app/src/main/java/com/safaashatha/allin1/productsdetails.kt
+++ app/src/main/java/com/safaashatha/allin1/productsdetails.kt
@@ -2,18 +2,27 @@ package com.safaashatha.allin1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.productsdetails.*
 
 class productsdetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.productsdetails)
-        val b=intent.extras!!
-        val prodname=b.getString("name")
-        val prodprice=b.getString("price")
-        val prodimg=b.getInt("image")
-        productsname.text=prodname
-        productsprice.text=prodprice
+        val b = intent.extras!!
+        val prodname = b.getString("name")
+        val prodprice = b.getString("price")
+        val prodimg = b.getInt("image")
+        productsname.text = prodname
+        productsprice.text = prodprice
         productsimg.setImageResource(prodimg)
     }
+
+   // FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(
+    //"shops"
+    //).child(FirebaseAuth.getInstance().currentUser!!.uid + "/products")
+    //.child(productsname.text.toString()).removeValue()
+
 }
