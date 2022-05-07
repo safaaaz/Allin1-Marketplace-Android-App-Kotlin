449a0ed3f5da30125804134b143dca8ca463c9f4
safaaaz <58644583+safaaaz@users.noreply.github.com>
2022-05-08 00:25:29 +0300

    COMM

---
+++ app/src/main/java/com/safaashatha/allin1/editprofileuser.kt
@@ -0,0 +1,185 @@
+package com.safaashatha.allin1
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.SearchView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_editprofileuser.*
+import kotlinx.android.synthetic.main.fragment_addprod.*
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.auth.api.signin.internal.Storage
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
+
+class editprofileuser : AppCompatActivity() {
+    lateinit var uriimage : Uri
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        uriimage= Uri.EMPTY
+        setContentView(R.layout.activity_editprofileuser)
+        changeimage.setOnClickListener{
+            openGalleryForImage()
+            //uploadimage()
+        }
+        saveprofile.setOnClickListener {
+            val firstname= FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(
+                "Users"
+            ).child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(user(FirebaseAuth.getInstance().currentUser!!.uid,
+                firstnameinput.text.toString(),
+                lastnameinput.text.toString(),
+                emailinput.text.toString(),
+                passwordinput.text.toString(),
+                birthdayinput.text.toString(),
+                addressinput.text.toString(),
+                phoneinput.text.toString()
+            ))
+            Toast.makeText(this,"yesssss",Toast.LENGTH_LONG).show()
+        }
+        image.setImageResource(R.drawable.googleg_standard_color_18)
+        val userid=intent.getStringExtra("user_id")
+        val firstname= FirebaseDatabase.getInstance("https://allin1-23085-default-rtdb.asia-southeast1.firebasedatabase.app").reference.child(
+            "Users"
+        ).child(FirebaseAuth.getInstance().currentUser!!.uid).get().addOnSuccessListener {
+
+            val k=it.getValue(user::class.java)
+            image.setImageResource(k!!.Image)
+            val filename=FirebaseAuth.getInstance().currentUser!!.uid.toString()
+            val storref=FirebaseStorage.getInstance().reference.child("profileimages/$filename")
+            val localfile=File.createTempFile("tempimage","jpg")
+            storref.getFile(localfile).addOnSuccessListener {
+                val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+                image.setImageBitmap(bitmap)
+            }
+            firstnameinput.setText(k!!.Firstname)
+            lastnameinput.setText(k!!.Lastname)
+            emailinput.setText(k!!.Email)
+            passwordinput.setText(k!!.Password.toString())
+            birthdayinput.setText(k!!.Birthday.toString())
+            addressinput.setText(k!!.Address)
+            phoneinput.setText(k!!.phone)
+
+            //image.setText(k!!.Firstname)
+
+            println("999999999999999999999999999999999999999999999999999999999"+k!!.Email)
+        }
+        if(firstname==null){
+            println("999999999999999999999999999999999999999999999999999999999")
+        }
+        println("......................................"+firstname.toString()!!)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu,menu)
+        val search = menu!!.findItem(R.id.prodsearch)
+        val searchView = search.actionView as SearchView
+        searchView.queryHint = "Search"
+        val cat = menu!!.findItem(R.id.categories)
+        cat.isEnabled=false
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //products.adapter.filter.filter("hhh")
+
+                return true
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+
+                startActivity(Intent(this,LoginActivity::class.java))
+                finish()
+                true
+            }
+            R.id.editprofile -> {
+                startActivity(Intent(this,editprofileuser::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 100)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 100){
+            println("2222222222222222222222222222222222222222222"+ data?.data!!.hashCode())
+            uriimage=data?.data!!
+            image.setImageURI(uriimage)
+            val progressgialog=ProgressDialog(this)
+            progressgialog.setMessage("uploading file...")
+            progressgialog.setCancelable(false)
+            progressgialog.show()
+            val filename=FirebaseAuth.getInstance().currentUser!!.uid.toString()
+            val storref=FirebaseStorage.getInstance().getReference("profileimages/$filename")
+            storref.putFile(uriimage).
+            addOnSuccessListener {
+                image.setImageURI(null)
+                Toast.makeText(this,"success",Toast.LENGTH_LONG)
+                if(progressgialog.isShowing) progressgialog.dismiss()
+            }.addOnFailureListener{
+                if(progressgialog.isShowing) progressgialog.dismiss()
+                Toast.makeText(this,"failed",Toast.LENGTH_LONG)
+
+            }
+            if(progressgialog.isShowing) progressgialog.dismiss()
+        }
+
+    }
+    /*
+    private fun uploadimage(){
+        val progressgialog=ProgressDialog(this)
+        progressgialog.setMessage("uploading file...")
+        progressgialog.setCancelable(false)
+        progressgialog.show()
+         val filename=FirebaseAuth.getInstance().currentUser!!.uid.toString()
+         val storref=FirebaseStorage.getInstance().getReference("images/$filename")
+        storref.putFile(uriimage).
+                addOnSuccessListener {
+                        image.setImageURI(null)
+                        Toast.makeText(this,"success",Toast.LENGTH_LONG)
+                        if(progressgialog.isShowing) progressgialog.dismiss()
+                }.addOnFailureListener{
+                        Toast.makeText(this,"failed",Toast.LENGTH_LONG)
+
+        }
+    }*/
+}
